7cc11f9692183ef7d4836de897aa1b12da682a38
koriebruh <koriebruh@gmail.com>
2025-04-03 16:50:23 +0700

    add billing service and setup generate java class from proto file

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
 mvn clean install
 mvn spring-boot:run
+
+# for generating java class from proto file, before that make sure you have installed
+# protoc, and add in some config and dependency in pom.xml
+mvn clean compile
